0812675f1923ddce4e71c9cedb546a090d074441
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2021-12-28 23:22:10 -0500

    Adding this missing routerModel forRoot code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,29 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     TopBarComponent
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {path: '', component: ProductListComponent}
+    ])
   ],
+
   providers: [],
-  bootstrap: [AppComponent]
+  
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
