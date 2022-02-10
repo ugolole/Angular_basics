8403ab76ba9b2187ae23cc33d2e51ad617a42976
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-02-10 15:32:51 -0500

    Adding notify event and lister to the product alerts component and product list component respectively

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import {Product } from '../products';
 
 @Component({
@@ -11,7 +11,10 @@ export class ProductAlertsComponent implements OnInit {
   constructor() { }
 
   // This indicates that the property values passes in from this components parent i.e. ProductListComponent.
-  @Input() product!: Product;
+  @Input() product!: Product | undefined;
+
+  // The @Output decorator allows this component to emit an event when the value of the notify property changes.
+  @Output() notify = new EventEmitter();
 
 
   ngOnInit(): void {
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { products } from '../products';
+import {Product, products} from '../products';
 
 @Component({
   selector: 'app-product-list',
@@ -22,5 +22,9 @@ export class ProductListComponent implements OnInit {
   // A method to share a product with other people I am guessing.
   share(){
     window.alert("This product has been shared!");
+  }
+
+  noNotify(product: Product){
+    window.alert(`You will be notified when product ${product.name} goes on sale`);
   }
 }
