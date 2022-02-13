79eb1dd0f3d8cb73001dce74c5a32c8048d50140
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-02-13 18:20:37 -0500

    Adding a product detail component with all the information needed.

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Product, products} from "../products";
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    // get the productId from the current route
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+
+    // Find the product that corresponds to this id
+    this.product = products.find(product => product.id = productIdFromRoute);
   }
 
 }
