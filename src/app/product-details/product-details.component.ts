9d2f7cc0906a8c59d72f97a2bf3a7a146df0c637
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-03-28 23:21:08 -0400

    add the cart service through the constructor of product details class

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Product, products} from "../products";
+import {CartService} from "../cart.service";
 
 @Component({
   selector: 'app-product-details',
@@ -11,7 +12,10 @@ export class ProductDetailsComponent implements OnInit {
 
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+    ) { }
 
   ngOnInit(): void {
 
@@ -19,9 +23,17 @@ export class ProductDetailsComponent implements OnInit {
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get('productId'));
 
-
     // Find the product that corresponds to this id
     this.product = products.find(product => product.id = productIdFromRoute);
+  }
+
+  // The add method which will add the current product to the cart
+  // This method will take the current product as an argument
+  // User the cart service's addToCart method to add the product to the cart.
+  // It will then display the message that you have now added the product to the cart.
+  addToCart(product: Product){
+    this.cartService.addToCart(product);
+    window.alert("Your " +  product.name + " has been added to the cart.")
   }
 
 }
